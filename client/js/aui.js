a516f6f31e976ecbd8077903e02326e80ba870ce
Grant Martin <commgdog@gmail.com>
2020-04-14 14:50:55 -0700

    Added us_states property for selects

--- client/js/aui.js
+++ client/js/aui.js
@@ -377,7 +377,7 @@ Vue.component('aui-select', {
                 @input="$emit('input', $event.target.value)" 
                 @change="$emit('change', $event.target.value)"
             >
-                <option v-for="(element, key) in data" :value="key" :selected="isSelected(key)">{{element}}</option>
+                <option v-for="(element, key) in options" :value="key" :selected="isSelected(key)">{{element}}</option>
             </select>
         </div>
         <div class="aui subtext" v-if="subtext" >{{subtext}}</div>
@@ -388,8 +388,7 @@ Vue.component('aui-select', {
             required: true
         },
         data: {
-            type: Array | Object,
-            required: true
+            type: Array | Object
         },
         value: {
             type: Array | Object | String | Boolean | Number
@@ -410,12 +409,86 @@ Vue.component('aui-select', {
         disabled: {
             type: Boolean,
             default: false
+        },
+        us_states: {
+            type: Boolean,
+            default: false
+        }
+    },
+    data() {
+        return {
+            options: {},
+            states: {
+                '0': '',
+                'AL': 'Alabama',
+                'AK': 'Alaska',
+                'AS': 'American Samoa',
+                'AZ': 'Arizona',
+                'AR': 'Arkansas',
+                'CA': 'California',
+                'CO': 'Colorado',
+                'CT': 'Connecticut',
+                'DE': 'Delaware',
+                'DC': 'District Of Columbia',
+                'FM': 'Federated States Of Micronesia',
+                'FL': 'Florida',
+                'GA': 'Georgia',
+                'GU': 'Guam',
+                'HI': 'Hawaii',
+                'ID': 'Idaho',
+                'IL': 'Illinois',
+                'IN': 'Indiana',
+                'IA': 'Iowa',
+                'KS': 'Kansas',
+                'KY': 'Kentucky',
+                'LA': 'Louisiana',
+                'ME': 'Maine',
+                'MH': 'Marshall Islands',
+                'MD': 'Maryland',
+                'MA': 'Massachusetts',
+                'MI': 'Michigan',
+                'MN': 'Minnesota',
+                'MS': 'Mississippi',
+                'MO': 'Missouri',
+                'MT': 'Montana',
+                'NE': 'Nebraska',
+                'NV': 'Nevada',
+                'NH': 'New Hampshire',
+                'NJ': 'New Jersey',
+                'NM': 'New Mexico',
+                'NY': 'New York',
+                'NC': 'North Carolina',
+                'ND': 'North Dakota',
+                'MP': 'Northern Mariana Islands',
+                'OH': 'Ohio',
+                'OK': 'Oklahoma',
+                'OR': 'Oregon',
+                'PW': 'Palau',
+                'PA': 'Pennsylvania',
+                'PR': 'Puerto Rico',
+                'RI': 'Rhode Island',
+                'SC': 'South Carolina',
+                'SD': 'South Dakota',
+                'TN': 'Tennessee',
+                'TX': 'Texas',
+                'UT': 'Utah',
+                'VT': 'Vermont',
+                'VI': 'Virgin Islands',
+                'VA': 'Virginia',
+                'WA': 'Washington',
+                'WV': 'West Virginia',
+                'WI': 'Wisconsin',
+                'WY': 'Wyoming'
+            }
         }
     },
     methods: {
        isSelected(key) {
            return key == this.value;
        }
+    },
+    mounted() {
+        this.options = this.us_states ? this.states : this.data;
     }
 });
 Vue.component('aui-datalist', {
