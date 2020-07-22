07937909545cc43cb9ff214726cc9c09bcd4b04e
Grant Martin <commgdog@gmail.com>
2020-07-22 10:56:58 -0700

    wysiwyg

--- components/Wysiwyg.vue
+++ components/Wysiwyg.vue
@@ -34,6 +34,7 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
                 type: String
             },
             text: {
+                type: String,
                 default: ''
             }
         },
