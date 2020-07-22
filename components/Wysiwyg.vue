978ca9f5e5ad772d2689a233b1c5b1a1d94628d3
Grant Martin <commgdog@gmail.com>
2020-07-22 10:49:16 -0700

    AUI wysiwyg

--- components/Wysiwyg.vue
+++ components/Wysiwyg.vue
@@ -17,8 +17,8 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 -->
 
 <template>
-    <div :id="id + '-container'" class="aui wysiwyg-container">
-        <label :for="id" class="wysiwyg-title">{{title}}</label>
+    <div :id="id + '-container'" class="aui wysiwyg">
+        <label>{{label}}</label>
         <div :id="id"></div>
     </div>
 </template>
@@ -30,7 +30,7 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
                 type: String,
                 required: true
             },
-            title: {
+            label: {
                 type: String
             },
             text: {
@@ -42,26 +42,23 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
                 editor: null
             }
         },
-        mounted: function() {
-            const comp = this;
-
-            comp.editor = pell.init({
-                element: document.getElementById(comp.id),
-                defaultParagraphSeparator: "p",
+        mounted() {
+            this.editor = pell.init({
+                element: document.getElementById(this.id),
+                defaultParagraphSeparator: 'p',
                 styleWithCSS: true,
                 onChange: html => {
-                    comp.$emit('input', html);
+                    this.$emit('input', html);
                 },
-                actions: ["bold", "italic", "underline", "heading1", "ulist", "olist"],
+                actions: ['bold', 'italic', 'underline', 'heading1', 'ulist', 'olist'],
                 classes: {
-                    actionbar: "pell-actionbar",
-                    button: "pell-btn",
-                    content: "pell-content",
-                    selected: "pell-button-selected",
+                    actionbar: 'pell-actionbar',
+                    button: 'pell-btn',
+                    content: 'pell-content',
+                    selected: 'pell-button-selected',
                 },
             });
-
-            comp.editor.content.innerHTML = comp.text;
+            this.editor.content.innerHTML = this.text;
         },
         watch: {
             text() {
@@ -73,60 +70,62 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
     }
 </script>
 
-
 <style lang="sass">
 
-    @import "../../../client/css/colors"
+/* Import the core DS colors */
+@import "../../../client/css/colors"
 
-    .aui.wysiwyg-container
+// Input container
+.aui.wysiwyg
+    margin: 1rem 0
 
-        .wysiwyg-title
-            display: block
-            margin-bottom: .5rem
+    /* Editor label */
+    label
+        display: block
+        margin-bottom: 0.5rem
 
-        .wysiwyg
-            border: 1px solid #ccc
-            border-radius: 4px
+    /* Editor action bar area */
+    .pell-actionbar
+        display: flex
+        background: #f9f9f9
+        border-top-left-radius: 0.25rem
+        border-top-right-radius: 0.25rem
+        border: 1px solid #ced4da
 
-        .pell-actionbar
+        /* Action bar buttons */
+        .pell-btn
             display: flex
-            background: #f9f9f9
-            border-top-left-radius: .25rem
-            border-top-right-radius: .25rem
-            border: 1px solid #ced4da
+            justify-content: center
+            align-content: center
+            font-size: 1.2rem
+            height: 2.5rem
+            line-height: 2.5rem
+            width: 2.5rem
+            background: none
+            border: none
 
-            .pell-btn
-                display: flex
-                justify-content: center
-                align-content: center
-                font-size: 1.2rem
-                height: 2.5rem
-                line-height: 2.5rem
-                width: 2.5rem
-                background: none
-                border: none
+            &:hover
+                cursor: pointer
+                background: darken(#f9f9f9, 10%)
 
-                &:hover
-                    cursor: pointer
-                    background: darken(#f9f9f9, 10%)
+    /* Editor content */
+    .pell-content
+        padding: 0.75rem
+        min-height: 4rem
+        border-left: 1px solid #ced4da
+        border-right: 1px solid #ced4da
+        border-bottom: 1px solid #ced4da
+        border-radius: 0 0 0.25rem 0.25rem
+        background: white
+        overflow-y: scroll
 
-        .pell-content
-            padding: .75rem
-            min-height: 4rem
-            border-left: 1px solid #ced4da
-            border-right: 1px solid #ced4da
-            border-bottom: 1px solid #ced4da
-            border-radius: 0 0 .25rem .25rem
-            background: white
-            overflow-y: scroll
+        &:focus
+            outline: none
+            box-shadow: 0 2px 5px 0 #ccc inset
 
-            &:focus
-                outline: none
-                box-shadow: 0 2px 5px -0px #ccc inset
-
-            p
-                margin: 0
-                padding: 0
-                overflow-wrap: break-spaces
+        p
+            margin: 0
+            padding: 0
+            overflow-wrap: break-spaces
 
 </style>
