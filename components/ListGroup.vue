1b3e57c6ebe362f5f5d9028361afbd04d4e790b2
Grant Martin <commgdog@gmail.com>
2020-09-22 17:48:44 -0700

    Added icons to list groups

--- components/ListGroup.vue
+++ components/ListGroup.vue
@@ -19,11 +19,12 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 <template>
     <ul class="aui list-group">
         <li v-for="(row, key) in rows" :key="key" @click="row.action">
+            <span v-if="row.icon" class="leading-icon"><i :class="row.icon"></i></span>
             <div class="text">
                 <div class="title">{{row.title}}</div>
                 <div v-if="row.subtext" class="subtext">{{row.subtext}}</div>
             </div>
-            <span class="icon"></span>
+            <span class="trailing-icon"></span>
         </li>
     </ul>
 </template>
@@ -44,6 +45,9 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
                             return false;
                         }
                         if (typeof value[i].action !== 'function') {
+                            return false;
+                        }
+                        if (typeof value[i].icon !== 'undefined' && typeof value[i].subtext !== 'string') {
                             return false;
                         }
                     }
@@ -72,11 +76,19 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
         padding: 1rem 3rem 1rem 1rem
         list-style: none
         display: flex
-        justify-content: space-between
         background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8' viewBox='0 0 8 8'%3e%3cpath fill='%23a2aeb9' d='M6.0848 3.9152l-3.3125 3.3125q-0.0848 0.0848-0.2009 0.0848t-0.2009-0.0848l-0.7411-0.7411q-0.0848-0.0848-0.0848-0.2009t0.0848-0.2009l2.3705-2.3705-2.3705-2.3705q-0.0848-0.0848-0.0848-0.2009t0.0848-0.2009l0.7411-0.7411q0.0848-0.0848 0.2009-0.0848t0.2009 0.0848l3.3125 3.3125q0.0848 0.0848 0.0848 0.2009t-0.0848 0.2009z'/%3e%3c/svg%3e")
         background-repeat: no-repeat
         background-position: right .5rem center
         background-size: 1.25rem
+
+        /* Leading icon if present */
+        .leading-icon
+            font-size: 2rem
+            color: #777
+
+            /* Padding when icon present */
+            & + .text
+                margin-left: 1rem
 
         /* Entry title */
         .title
