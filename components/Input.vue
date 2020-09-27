fd9b317569afa0e15df66a596d303e76e3192918
mryan <hatduck@gmail.com>
2020-09-27 02:18:14 -0700

    Added smooth scrolling functionality to scroll to the earliest error on inputs in the page

--- components/Input.vue
+++ components/Input.vue
@@ -224,7 +224,28 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
         },
         watch: {
             failure() {
-                this.$refs.scrollAnchor.scrollIntoView({behavior: "smooth", block: "nearest"});
+
+                const scroll_callback = () => {
+
+                    let view = document.getElementById('ds-view');
+                    let el = document.querySelectorAll('.aui.input .border-failure')[0]
+
+                    if (el) {
+
+                        let box = el.getBoundingClientRect()
+                        let scroll_position = box.top - box.height - 100
+
+                        view.scrollBy({
+                            top: scroll_position,
+                            behavior: 'smooth'
+                        });
+
+                    }
+                }
+
+                clearTimeout(window.aui_failure_timeout);
+                window.aui_failure_timeout = setTimeout(scroll_callback, 99);
+
             }
         }
     }
