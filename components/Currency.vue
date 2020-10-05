d7e84fabc6dfa760b41bcab0151ea7c84b105865
mryan <hatduck@gmail.com>
2020-10-05 09:08:24 -0700

    Currency component ready for use

---
+++ components/Currency.vue
@@ -0,0 +1,34 @@
+<template>
+    <span class="aui currency" :class="negativeClass">
+        {{prettyValue}}
+    </span>
+</template>
+
+<script>
+export default {
+    props: {
+        value: {
+            type: String
+        }
+    },
+    computed: {
+        negativeClass() {
+            return (this.value >= 0) ? '' : 'negative';
+        },
+        prettyValue() {
+            let abs = Math.abs(parseFloat(this.value)).toFixed(2);
+            return (this.value >= 0) ? '$' + abs  : '-$' + abs;
+        }
+    }
+}
+</script>
+
+<style lang="sass">
+
+/* Import the core DS colors */
+@import "../../../client/css/colors"
+
+.negative
+    color: $failure
+
+</style>
