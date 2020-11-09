c3504a31ed123dc8414bd8a132d35d2e8a9de7e0
Grant Martin <commgdog@gmail.com>
2020-11-09 05:37:26 -0800

    Allow imports for header buttons

--- components/Crud.vue
+++ components/Crud.vue
@@ -49,7 +49,7 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 
                 <!-- List action area -->
                 <div class="action">
-                    <slot name="list-buttons"></slot>
+                    <slot name="list-buttons" :readList="readList"></slot>
                     <aui-button
                         v-if="list_show_create_button"
                         :disabled="state.overlay"
