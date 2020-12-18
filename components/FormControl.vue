a21fcfca00b0b9d631c17543afa468a6ae35e9a2
Grant Martin <commgdog@gmail.com>
2020-12-17 22:15:23 -0800

    Justify form control elements to center

--- components/FormControl.vue
+++ components/FormControl.vue
@@ -295,7 +295,10 @@ export default {
 .aui.form-control
     display: flex
     flex-direction: column
-    justify-content: center
+
+    /* Center inline items */
+    &.checkbox, &.toggle, &.radio
+        justify-content: center
 
     /* Add margin to the label text if present */
     &.input, &.select, &.textarea, &.wysiwyg
