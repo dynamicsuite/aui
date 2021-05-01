0ddf49d0c4eb7b189ffeb03ebff325d5bcc7573f
Grant Martin <commgdog@gmail.com>
2021-05-01 11:26:21 -0700

    update pagination

--- client/components/Pagination.vue
+++ client/components/Pagination.vue
@@ -11,253 +11,263 @@ file that was distributed with this source code.
 -->
 
 <template>
-    <div class="aui pagination">
+  <div class="aui pagination">
 
-        <!-- Display range text -->
-        <div class="range">
-            <label>Showing</label>
-            <select :disabled="disabled" @change="updateLimit">
-                <option
-                    v-for="limit in list_range_limit"
-                    :key="'page-limit-' + limit"
-                    :value="limit"
-                    v-html="limit"
-                />
-            </select>
-            <span>from {{from.toLocaleString()}} to {{to.toLocaleString()}} of {{total.toLocaleString()}}</span>
-        </div>
+    <!-- Display range text -->
+    <div class="range">
+      <label>Showing</label>
+      <select :disabled="disabled" @change="updateLimit">
+        <option
+          v-for="limit in list_range_limit"
+          :key="'page-limit-' + limit"
+          :value="limit"
+          v-html="limit"
+        />
+      </select>
+      <span>from {{from.toLocaleString()}} to {{to.toLocaleString()}} of {{total.toLocaleString()}}</span>
+    </div>
 
-        <!-- Pagination buttons  -->
-        <div v-if="pages > 1" class="buttons">
+    <!-- Pagination buttons  -->
+    <div v-if="pages > 1" class="aui btn-group">
 
-            <!-- Left Chevrons -->
-            <aui-button
-                type="secondary"
-                class="chevron"
-                text="«"
-                :disabled="disabled"
-                @click="paginate(1)"
-            />
-            <aui-button
-                type="secondary"
-                class="chevron"
-                text="‹"
-                :disabled="disabled"
-                @click="paginate(page - 1)"
-            />
+      <!-- Left Chevrons -->
+      <aui-button
+        type="secondary"
+        class="chevron"
+        text="«"
+        :disabled="disabled"
+        @click="paginate(1)"
+      />
+      <aui-button
+        type="secondary"
+        class="chevron"
+        text="‹"
+        :disabled="disabled"
+        @click="paginate(page - 1)"
+      />
 
-            <!-- Page buttons -->
-            <aui-button
-                v-for="(page, key) in button_range"
-                :key="'pagination-' + key"
-                :type="paginateButtonType(page)"
-                :text="page"
-                :disabled="disabled"
-                class="page-button"
-                @click="paginate(page)"
-            />
+      <!-- Page buttons -->
+      <aui-button
+        v-for="(page, key) in button_range"
+        :key="'pagination-' + key"
+        :type="paginateButtonType(page)"
+        :text="page"
+        :disabled="disabled"
+        class="page-button"
+        @click="paginate(page)"
+      />
 
-            <!-- Right Chevrons -->
-            <aui-button
-                type="secondary"
-                class="chevron"
-                text="›"
-                :disabled="disabled"
-                @click="paginate(page + 1)"
-            />
-            <aui-button
-                type="secondary"
-                class="chevron"
-                text="»"
-                :disabled="disabled"
-                @click="paginate(pages)"
-            />
-
-        </div>
-
-        <!-- Single page display -->
-        <div v-else class="single-page"><strong>1</strong> / <strong>1</strong></div>
+      <!-- Right Chevrons -->
+      <aui-button
+        type="secondary"
+        class="chevron"
+        text="›"
+        :disabled="disabled"
+        @click="paginate(page + 1)"
+      />
+      <aui-button
+        type="secondary"
+        class="chevron"
+        text="»"
+        :disabled="disabled"
+        @click="paginate(pages)"
+      />
 
     </div>
+
+    <!-- Single page display -->
+    <div v-else class="single-page"><strong>1</strong> / <strong>1</strong></div>
+
+  </div>
 </template>
 
 <script>
 // noinspection JSValidateTypes
 export default {
-    props: {
+  props: {
 
-        /**
-         * If the buttons are disable and non-interactive.
-         */
-        disabled: {
-            type: Boolean,
-            default: false
-        },
-
-        /**
-         * The current page.
-         */
-        page: {
-            type: Number,
-            required: true
-        },
-
-        /**
-         * The total number of potential records in the dataset.
-         */
-        total: {
-            type: Number,
-            required: true
-        },
-
-        /**
-         * The record limit per page.
-         */
-        limit: {
-            type: Number,
-            required: true
-        },
-
-        /**
-         * The possible limit options for the dropdown.
-         */
-        list_range_limit: {
-            type: Array,
-            required: true
-        }
-
+    /**
+     * If the buttons are disable and non-interactive.
+     *
+     * @type {boolean}
+     */
+    disabled: {
+      type: Boolean,
+      default: false
     },
-    computed: {
 
-        /**
-         * The total number of available pages.
-         *
-         * @returns {number}
-         */
-        pages() {
-            return Math.ceil(this.total / this.limit);
-        },
-
-        /**
-         * The offset value of current page.
-         *
-         * @returns {number}
-         */
-        offset() {
-            let offset = (this.page - 1) * this.limit;
-            if (offset > this.total) {
-                offset = this.total - this.limit
-                this.$emit('update:page', this.pages);
-            }
-            return offset;
-        },
-
-        /**
-         * The beginning of the viewable range.
-         *
-         * @returns {number}
-         */
-        from() {
-            return this.offset + 1;
-        },
-
-        /**
-         * Ending of the viewable range.
-         *
-         * @returns {number}
-         */
-        to() {
-            let to = this.from + this.limit - 1;
-            if (to > this.total) {
-                to = this.total;
-            }
-            return to;
-        },
-
-        /**
-         * The range of buttons to render.
-         *
-         * Each button represents it's respective page that clicking will paginate to.
-         *
-         * @returns {number[]}
-         */
-        button_range() {
-            const range = [];
-            if (this.pages < 5) {
-                for (let i = 1; i <= this.pages; i++) {
-                    range.push(i);
-                }
-            } else if (this.page < 3) {
-                for (let i = 1; i <= 5; i++) {
-                    range.push(i);
-                }
-            } else {
-                const at_end = this.page + 2 > this.pages
-                const offset = !at_end ? this.page + 2 : this.pages;
-                for (let i = at_end ? this.pages - 4 : this.page - 2; i <= offset; i++) {
-                    range.push(i);
-                }
-            }
-            return range;
-        }
-
+    /**
+     * The current page.
+     *
+     * @type {number}
+     */
+    page: {
+      type: Number,
+      required: true
     },
-    methods: {
 
-        /**
-         * Update the limit from the range limits.
-         *
-         * @param {object} event - The change event handler.
-         * @returns {undefined}
-         */
-        updateLimit(event) {
-            this.$emit('update:limit', parseInt(event.target.value));
-            this.$emit('update:page', 1);
-            this.$emit('paginate');
-        },
+    /**
+     * The total number of potential records in the dataset.
+     *
+     * @type {number}
+     */
+    total: {
+      type: Number,
+      required: true
+    },
 
-        /**
-         * Emit a page back to the parent to be paginated.
-         *
-         * @param {Number} page - The page to paginate to.
-         * @returns {undefined}
-         */
-        paginate(page) {
+    /**
+     * The record limit per page.
+     *
+     * @type {number}
+     */
+    limit: {
+      type: Number,
+      required: true
+    },
 
-            // Do not allow pagination when disabled
-            if (this.disabled) {
-                return;
-            }
-
-            // Ignore current page clicks
-            if (page === this.page) {
-                return;
-            }
-
-            // Do not paginate out of range
-            if (page < 1 || page > this.pages) {
-                return;
-            }
-
-            // Emit the pagination to the given page
-            this.$emit('update:page', page);
-            this.$emit('paginate');
-
-        },
-
-        /**
-         * Get the button type for the given pagination button page.
-         *
-         * The button for the current page should be primary, all others should be secondary.
-         *
-         * @param {Number|String} page - The page number.
-         * @return {string}
-         */
-        paginateButtonType(page) {
-            return this.page === parseInt(page) ? 'primary' : 'secondary';
-        }
-
+    /**
+     * The possible limit options for the dropdown.
+     *
+     * @type {number[]}
+     */
+    list_range_limit: {
+      type: Array,
+      required: true
     }
+
+  },
+  computed: {
+
+    /**
+     * The total number of available pages.
+     *
+     * @returns {number}
+     */
+    pages() {
+      return Math.ceil(this.total / this.limit);
+    },
+
+    /**
+     * The offset value of current page.
+     *
+     * @returns {number}
+     */
+    offset() {
+      let offset = (this.page - 1) * this.limit;
+      if (offset > this.total) {
+        offset = this.total - this.limit
+        this.$emit('update:page', this.pages);
+      }
+      return offset;
+    },
+
+    /**
+     * The beginning of the viewable range.
+     *
+     * @returns {number}
+     */
+    from() {
+      return this.offset + 1;
+    },
+
+    /**
+     * Ending of the viewable range.
+     *
+     * @returns {number}
+     */
+    to() {
+      let to = this.from + this.limit - 1;
+      if (to > this.total) {
+        to = this.total;
+      }
+      return to;
+    },
+
+    /**
+     * The range of buttons to render.
+     *
+     * Each button represents it's respective page that clicking will paginate to.
+     *
+     * @returns {number[]}
+     */
+    button_range() {
+      const range = [];
+      if (this.pages < 5) {
+        for (let i = 1; i <= this.pages; i++) {
+          range.push(i);
+        }
+      } else if (this.page < 3) {
+        for (let i = 1; i <= 5; i++) {
+          range.push(i);
+        }
+      } else {
+        const at_end = this.page + 2 > this.pages
+        const offset = !at_end ? this.page + 2 : this.pages;
+        for (let i = at_end ? this.pages - 4 : this.page - 2; i <= offset; i++) {
+          range.push(i);
+        }
+      }
+      return range;
+    }
+
+  },
+  methods: {
+
+    /**
+     * Update the limit from the range limits.
+     *
+     * @param {object} event - The change event handler.
+     * @returns {undefined}
+     */
+    updateLimit(event) {
+      this.$emit('update:limit', parseInt(event.target.value));
+      this.$emit('update:page', 1);
+      this.$emit('paginate');
+    },
+
+    /**
+     * Emit a page back to the parent to be paginated.
+     *
+     * @param {number} page - The page to paginate to.
+     * @returns {undefined}
+     */
+    paginate(page) {
+
+      // Do not allow pagination when disabled
+      if (this.disabled) {
+        return;
+      }
+
+      // Ignore current page clicks
+      if (page === this.page) {
+        return;
+      }
+
+      // Do not paginate out of range
+      if (page < 1 || page > this.pages) {
+        return;
+      }
+
+      // Emit the pagination to the given page
+      this.$emit('update:page', page);
+      this.$emit('paginate');
+
+    },
+
+    /**
+     * Get the button type for the given pagination button page.
+     *
+     * The button for the current page should be primary, all others should be secondary.
+     *
+     * @param {number | string} page - The page number.
+     * @return {string}
+     */
+    paginateButtonType(page) {
+      return this.page === parseInt(page) ? 'primary' : 'secondary';
+    }
+
+  }
 }
 </script>
 
@@ -268,98 +278,81 @@ export default {
 
 /* Pagination container */
 .aui.pagination
-    display: flex
-    align-items: center
-    justify-content: center
-    flex-wrap: wrap
+  display: flex
+  align-items: center
+  justify-content: center
+  flex-wrap: wrap
 
-    /* Split range and buttons on mobile view */
+  /* Split range and buttons on mobile view */
+  @include on-mobile-view
+    flex-direction: column
+
+  /* Range display */
+  .range
+    white-space: nowrap
+
+    /* Lower font size for ipads and lower */
+    @include on-ipad-view
+      font-size: 0.9rem
+
+    /* Separate from buttons on mobile view */
     @include on-mobile-view
-        flex-direction: column
+      display: flex
+      flex-direction: column
+      margin-bottom: 0.5rem
 
-    /* Range display */
-    .range
-        color: #111
-        white-space: nowrap
+      /* Align rows */
+      label, select
+        text-align: center
+        margin-bottom: 0.5rem !important
 
-        /* Lower font size for ipads and lower */
-        @include on-ipad-view
-            font-size: 0.9rem
+    /* Select for limit */
+    select
+      height: 2rem
+      padding: 0 0.25rem
+      margin: 0 0.35rem
+      border-radius: 0.25rem
+      border: 1px solid $color-border
+      text-align-last: center
+      background: $color-text-inverted
 
-        /* Separate from buttons on mobile view */
-        @include on-mobile-view
-            display: flex
-            flex-direction: column
-            margin-bottom: 0.5rem
+      /* When disabled */
+      &:disabled
+        cursor: not-allowed
+        background: darken($color-border, 5%) !important
 
-            /* Align rows */
-            label, select
-                text-align: center
-                margin-bottom: 0.5rem !important
+      /* Remove chrome outline */
+      &:focus
+        outline: none
 
-        /* Select for limit */
-        select
-            height: 2rem
-            padding: 0 0.25rem
-            margin: 0 0.35rem
-            border-radius: 0.25rem
-            border: 1px solid #ced4da
-            text-align-last: center
+  /* Pagination buttons */
+  .btn-group
+    display: flex
+    margin-left: auto
 
-            /* When disabled */
-            &:disabled
-                cursor: not-allowed
-                background: darken(#e9ecef, 15%)
+    /* Mobile buttons */
+    @include on-mobile-view
+      margin-left: 0
 
-            /* Remove chrome outline */
-            &:focus
-                outline: none
+      /* Buttons on mobile */
+      .page-button:not(.primary)
+        display: none
 
     /* Pagination buttons */
-    .buttons
-        display: flex
-        margin-left: auto
+    .btn
+      height: 1rem
+      font-size: 0.8rem
 
-        /* Mobile buttons */
-        @include on-mobile-view
-            margin-left: 0
+      /* Chevron buttons */
+      &.chevron
+        font-size: 1rem
 
-            /* Buttons on mobile */
-            .page-button:not(.btn-primary)
-                display: none
+  /* Single page replacement for buttons */
+  .single-page
+    display: flex
+    margin-left: auto
 
-        /* Pagination buttons */
-        .btn
-            display: flex
-            justify-content: center
-            align-items: center
-            margin: 0
-            padding: 0.5rem
-            font-size: 0.8rem
-            height: 2rem
-            min-width: 1.9rem
-
-            /* Chevron buttons */
-            &.chevron
-                font-size: 1rem
-
-            /* Remove radius for inner buttons and add separating border */
-            &:not(:last-of-type)
-                border-top-right-radius: 0
-                border-bottom-right-radius: 0
-                border-right: 1px solid rgba(0, 0, 0, 0.15)
-
-            /* Remove radius for inner buttons */
-            &:not(:first-of-type)
-                border-top-left-radius: 0
-                border-bottom-left-radius: 0
-
-    /* Single page replacement for buttons */
-    .single-page
-        display: flex
-        margin-left: auto
-
-        @include on-mobile-view
-            display: none
+    @include on-mobile-view
+      display: none
 
 </style>
