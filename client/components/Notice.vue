bf68aa9683863e8ad0cdbc43811fe2aad43b13a2
Grant Martin <commgdog@gmail.com>
2021-05-01 10:55:11 -0700

    Update notices

--- client/components/Notice.vue
+++ client/components/Notice.vue
@@ -1,86 +1,88 @@
 <!--
-Aui Package
-Copyright (C) 2020 Dynamic Suite Team
+This file is part of the Dynamic Suite Docs package.
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation version 3.
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software Foundation,
-Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+@package DynamicSuite\Docs
+@author Grant Martin <commgdog@gmail.com>
+@copyright 2021 Dynamic Suite Team
 -->
 
 <template>
-    <div class="aui notice">
-        <i :class="icon_classes"></i>
-        <h4><slot>{{text}}</slot></h4>
-        <span v-if="subtext" v-html="subtext"></span>
-    </div>
+  <div class="aui notice">
+    <i :class="icon_classes"></i>
+    <h4><slot>{{text}}</slot></h4>
+    <span v-if="subtext" v-html="subtext"></span>
+  </div>
 </template>
 
 <script>
+// noinspection JSValidateTypes
 export default {
-    props: {
+  props: {
 
-        /**
-         * Notice type which defined the color of the notice.
-         */
-        type: {
-            type: String,
-            default: 'secondary',
-            validator: value => {
-                return ['primary', 'secondary', 'success', 'warning', 'failure'].indexOf(value) !== -1;
-            }
-        },
-
-        /**
-         * The Font Awesome icon to display for the notice.
-         */
-        icon: {
-            type: String,
-            default: 'fas fa-cog',
-        },
-
-        /**
-         * The text to display under the icon.
-         *
-         * This can be set with this property or using the default slot.
-         */
-        text: {
-            type: String | Number | null,
-            default: null
-        },
-
-        /**
-         * Subtext to display under the notice text.
-         */
-        subtext: {
-            type: String | Number | null,
-            default: null
-        }
-
+    /**
+     * Notice type which defined the color of the notice.
+     *
+     * @type {string}
+     */
+    type: {
+      type: String,
+      default: 'secondary',
+      validator: value => {
+        return ['primary', 'secondary', 'success', 'warning', 'failure'].indexOf(value) !== -1;
+      }
     },
-    computed: {
 
-        /**
-         * Classes to apply to the icon.
-         *
-         * @returns {object}
-         */
-        icon_classes() {
-            return {
-                [this.icon]: true,
-                [this.type]: true
-            };
-        }
+    /**
+     * The Font Awesome icon to display for the notice.
+     *
+     * @type {string}
+     */
+    icon: {
+      type: String,
+      default: 'fas fa-cog',
+    },
 
+    /**
+     * The text to display under the icon.
+     *
+     * This can be set with this property or using the default slot.
+     *
+     * @type {string | number | null}
+     */
+    text: {
+      type: String | Number | null,
+      default: null
+    },
+
+    /**
+     * Subtext to display under the notice text.
+     *
+     * @type {string | number | null}
+     */
+    subtext: {
+      type: String | Number | null,
+      default: null
     }
+
+  },
+  computed: {
+
+    /**
+     * Classes to apply to the icon.
+     *
+     * @returns {object}
+     */
+    icon_classes() {
+      return {
+        [this.icon]: true,
+        [this.type]: true
+      };
+    }
+
+  }
 }
 </script>
 
@@ -91,41 +93,40 @@ export default {
 
 /* Notice container */
 .aui.notice
-    display: flex
-    flex-direction: column
-    justify-content: center
-    align-items: center
+  display: flex
+  flex-direction: column
+  justify-content: center
+  align-items: center
 
-    /* Icon */
-    i
-        font-size: 3rem
-        margin-bottom: 0.5rem
+  /* Icon */
+  i
+    font-size: 3rem
+    margin-bottom: 0.5rem
 
-        &.primary
-            color: $color-primary
+    &.primary
+      color: $color-primary
 
-        &.secondary
-            color: lighten($color-secondary, 20%)
+    &.secondary
+      color: lighten($color-secondary, 20%)
 
-        &.success
-            color: $color-success
+    &.success
+      color: $color-success
 
-        &.warning
-            color: $color-warning
+    &.warning
+      color: $color-warning
 
-        &.failure
-            color: $color-failure
+    &.failure
+      color: $color-failure
 
-    /* Text */
-    h4
-        color: lighten(#000, 35%)
+  /* Text */
+  h4
+    margin: 0
+    color: $color-text-soft
 
-        &:last-child
-            margin-bottom: 0
-
-    /* Subtext */
-    span
-        font-size: 0.9rem
-        color: lighten(#000, 45%)
+  /* Subtext */
+  span
+    margin-top: 0.25rem
+    font-size: 0.9rem
+    color: $color-text-softest
 
 </style>
