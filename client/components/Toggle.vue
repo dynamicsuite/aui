99ab9dc05995f08168d5cdcb34bd7022b3096ff8
Grant Martin <commgdog@gmail.com>
2021-05-01 12:32:44 -0700

    update WYSIWYG

--- client/components/Toggle.vue
+++ client/components/Toggle.vue
@@ -11,118 +11,133 @@ file that was distributed with this source code.
 -->
 
 <template>
-    <aui-form-control v-bind="properties">
-        <input
-            type="checkbox"
-            :value="value"
-            :checked="toggled"
-            :disabled="disabled"
-            @input="$emit('input', $event.target.checked)"
-        />
-    </aui-form-control>
+  <aui-form-control v-bind="properties">
+    <input
+      type="checkbox"
+      :value="value"
+      :checked="toggled"
+      :disabled="disabled"
+      @input="$emit('input', $event.target.checked)"
+    />
+  </aui-form-control>
 </template>
 
 <script>
 // noinspection JSValidateTypes
 export default {
-    props: {
+  props: {
 
-        /**
-         * Toggle label.
-         *
-         * This is an alias for the slot content when using plaintext. Slot should be used if custom HTML is
-         * required.
-         */
-        label: {
-            type: String | null,
-            default: null
-        },
-
-        /**
-         * The model binding value of the toggle.
-         */
-        value: {
-            type: String | Number | Boolean | null,
-            default: null
-        },
-
-        /**
-         * If the toggle is toggled by default.
-         */
-        toggled: {
-            type: Boolean | Number,
-            default: false
-        },
-
-        /**
-         * If the toggle is disabled and non-interactive.
-         */
-        disabled: {
-            type: Boolean,
-            default: false
-        },
-
-        /**
-         * Success subtext to display under the toggle.
-         *
-         * Subtext values override each other in the following order, only one may be present at a time:
-         *
-         * success > failure > subtext
-         */
-        success: {
-            type: String | null,
-            default: null
-        },
-
-        /**
-         * Failure subtext to display under the toggle.
-         *
-         * Subtext values override each other in the following order, only one may be present at a time:
-         *
-         * success > failure > subtext
-         */
-        failure: {
-            type: String | null,
-            default: null
-        },
-
-        /**
-         * Subtext to display under the toggle.
-         *
-         * Subtext values override each other in the following order, only one may be present at a time:
-         *
-         * success > failure > subtext
-         */
-        subtext: {
-            type: String | null,
-            default: null
-        }
-
+    /**
+     * Toggle label.
+     *
+     * This is an alias for the slot content when using plaintext.
+     *
+     * Slot should be used if custom HTML is required.
+     *
+     * @type {string | null}
+     */
+    label: {
+      type: String | null,
+      default: null
     },
-    computed: {
 
-        /**
-         * Properties to pass down to the form control component.
-         *
-         * @returns {
-         *     {parent: string},
-         *     {label: string},
-         *     {success: string},
-         *     {failure: string},
-         *     {subtext: string}
-         * }
-         */
-        properties() {
-            return {
-                parent: 'toggle',
-                label: this.$slots.default ? this.$slots.default : this.label,
-                success: this.success,
-                failure: this.failure,
-                subtext: this.subtext
-            };
-        }
+    /**
+     * The model binding value of the toggle.
+     *
+     * @type {string | number | boolean | null}
+     */
+    value: {
+      type: String | Number | Boolean | null,
+      default: null
+    },
 
+    /**
+     * If the toggle is toggled by default.
+     *
+     * @type {boolean | number}
+     */
+    toggled: {
+      type: Boolean | Number,
+      default: false
+    },
+
+    /**
+     * If the toggle is disabled and non-interactive.
+     *
+     * @type {boolean}
+     */
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+
+    /**
+     * Success subtext to display under the toggle.
+     *
+     * Subtext values override each other in the following order, only one may be present at a time:
+     *
+     * success > failure > subtext
+     *
+     * @type {string | null}
+     */
+    success: {
+      type: String | null,
+      default: null
+    },
+
+    /**
+     * Failure subtext to display under the toggle.
+     *
+     * Subtext values override each other in the following order, only one may be present at a time:
+     *
+     * success > failure > subtext
+     *
+     * @type {string | null}
+     */
+    failure: {
+      type: String | null,
+      default: null
+    },
+
+    /**
+     * Subtext to display under the toggle.
+     *
+     * Subtext values override each other in the following order, only one may be present at a time:
+     *
+     * success > failure > subtext
+     *
+     * @type {string | null}
+     */
+    subtext: {
+      type: String | null,
+      default: null
     }
+
+  },
+  computed: {
+
+    /**
+     * Properties to pass down to the form control component.
+     *
+     * @returns {
+     *   {parent: string},
+     *   {label: string},
+     *   {success: string},
+     *   {failure: string},
+     *   {subtext: string}
+     * }
+     */
+    properties() {
+      return {
+        parent: 'toggle',
+        label: this.$slots.default ? this.$slots.default : this.label,
+        success: this.success,
+        failure: this.failure,
+        subtext: this.subtext
+      };
+    }
+
+  }
 }
 </script>
 
@@ -131,41 +146,41 @@ export default {
 /* Toggle structure */
 .aui.toggle
 
-    /* Pseudo element */
-    .pseudo
-        width: 56px !important
-        height: 28px !important
-        cursor: pointer
-        top: 0
-        left: 0
-        right: 0
-        bottom: 0
-        background: #ccc
-        -webkit-transition: 0.4s
-        transition: 0.4s
-        border-radius: 34px
+  /* Pseudo element */
+  .pseudo
+    width: 56px !important
+    height: 28px !important
+    cursor: pointer
+    top: 0
+    left: 0
+    right: 0
+    bottom: 0
+    background: #ccc
+    -webkit-transition: 0.4s
+    transition: 0.4s
+    border-radius: 34px
 
-        /* Slider circle */
-        &:before
-            position: absolute
-            content: ""
-            height: 20px
-            width: 20px
-            left: 4px
-            bottom: 4px
-            background: white
-            -webkit-transition: 0.4s
-            transition: 0.4s
-            border-radius: 50%
+    /* Slider circle */
+    &:before
+      position: absolute
+      content: ""
+      height: 20px
+      width: 20px
+      left: 4px
+      bottom: 4px
+      background: white
+      -webkit-transition: 0.4s
+      transition: 0.4s
+      border-radius: 50%
 
-    /* Slider transition */
-    input:checked ~ .pseudo:before
-        -webkit-transform: translateX(28px)
-        -ms-transform: translateX(28px)
-        transform: translateX(28px)
+  /* Slider transition */
+  input:checked ~ .pseudo:before
+    -webkit-transform: translateX(28px)
+    -ms-transform: translateX(28px)
+    transform: translateX(28px)
 
-    /* Offset subtext */
-    .subtext
-        margin-left: calc(56px + 0.5rem)
+  /* Offset subtext */
+  .subtext
+    margin-left: calc(56px + 0.5rem)
 
 </style>
--- client/components/Wysiwyg.vue
+++ client/components/Wysiwyg.vue
@@ -12,200 +12,223 @@ file that was distributed with this source code.
 
 <template>
   <aui-form-control v-bind="properties">
-      <div ref="editor" />
+    <div ref="editor" />
   </aui-form-control>
 </template>
 
 <script>
 // noinspection JSValidateTypes
 export default {
-    props: {
+  props: {
 
-        /**
-         * WYSIWYG label.
-         *
-         * This is an alias for the slot content when using plaintext. Slot should be used if custom HTML is
-         * required.
-         */
-        label: {
-          type: String | null,
-          default: null
-        },
-
-        /**
-         * Pell action bar actions.
-         */
-        actions: {
-            type: Array,
-            default: () => ['bold', 'italic', 'underline', 'heading1', 'ulist', 'olist']
-        },
-
-        /**
-         * Default HTML tag separator for paragraphs.
-         */
-        default_paragraph_separator: {
-            type: String,
-            default: 'p'
-        },
-
-        /**
-         * If the content should be styled with CSS vs tags.
-         */
-        style_with_css: {
-            type: Boolean,
-            default: true
-        },
-
-        /**
-         * Custom Pell widget classes.
-         */
-        classes: {
-            type: Object,
-            default: () => {
-                return {
-                    actionbar: 'pell-actionbar',
-                    button: 'pell-btn',
-                    content: 'pell-content',
-                    selected: 'pell-button-selected'
-                }
-            }
-        },
-
-        /**
-         * The model binding value of the WYSIWYG.
-         */
-        value: {
-            type: String | Number | Boolean | null,
-            default: null
-        },
-
-        /**
-         * WYSIWYG placeholder text.
-         */
-        placeholder: {
-            type: String | null,
-            default: null
-        },
-
-        /**
-         * If the WYSIWYG is disabled and non-interactive.
-         */
-        disabled: {
-            type: Boolean,
-            default: false
-        },
-
-        /**
-         * Success subtext to display under the WYSIWYG.
-         *
-         * Subtext values override each other in the following order, only one may be present at a time:
-         *
-         * success > failure > subtext
-         */
-        success: {
-            type: String | null,
-            default: null
-        },
-
-        /**
-         * Failure subtext to display under the WYSIWYG.
-         *
-         * Subtext values override each other in the following order, only one may be present at a time:
-         *
-         * success > failure > subtext
-         */
-        failure: {
-            type: String | null,
-            default: null
-        },
-
-        /**
-         * Subtext to display under the WYSIWYG.
-         *
-         * Subtext values override each other in the following order, only one may be present at a time:
-         *
-         * success > failure > subtext
-         */
-        subtext: {
-            type: String | null,
-            default: null
-        }
-
+    /**
+     * WYSIWYG label.
+     *
+     * This is an alias for the slot content when using plaintext.
+     *
+     * Slot should be used if custom HTML is required.
+     *
+     * @type {string | null}
+     */
+    label: {
+      type: String | null,
+      default: null
     },
-    data() {
+
+    /**
+     * Pell action bar actions.
+     *
+     * @type {string[]}
+     */
+    actions: {
+      type: Array,
+      default: () => ['bold', 'italic', 'underline', 'heading1', 'ulist', 'olist']
+    },
+
+    /**
+     * Default HTML tag separator for paragraphs.
+     *
+     * @type {string}
+     */
+    default_paragraph_separator: {
+      type: String,
+      default: 'p'
+    },
+
+    /**
+     * If the content should be styled with CSS vs tags.
+     *
+     * @type {boolean}
+     */
+    style_with_css: {
+      type: Boolean,
+      default: true
+    },
+
+    /**
+     * Custom Pell widget classes.
+     *
+     * @type {object}
+     */
+    classes: {
+      type: Object,
+      default: () => {
         return {
-            editor: null
-        };
-    },
-    computed: {
-
-        /**
-         * Properties to pass down to the form control component.
-         *
-         * @returns {
-         *     {parent: string},
-         *     {label: string},
-         *     {success: string},
-         *     {failure: string},
-         *     {subtext: string}
-         * }
-         */
-        properties() {
-            return {
-                parent: 'wysiwyg',
-                label: this.$slots.default ? this.$slots.default : this.label,
-                success: this.success,
-                failure: this.failure,
-                subtext: this.subtext
-            };
+          actionbar: 'pell-actionbar',
+          button: 'pell-btn',
+          content: 'pell-content',
+          selected: 'pell-button-selected'
         }
-
+      }
     },
-    watch: {
 
-        /**
-         * Update the value on model changes.
-         *
-         * @param {string} value - The new value.
-         * @returns {undefined}
-         */
-        value: function (value) {
-            if (this.editor.content.innerHTML !== value) {
-                this.editor.content.innerHTML = value;
-            }
-        },
-
-        /**
-         * Adjust the disabled state.
-         *
-         * @param {string} value - The new disabled state.
-         * @returns {undefined}
-         */
-        disabled: function (value) {
-            this.$refs.editor
-                .querySelector('.pell-content')
-                .setAttribute('contenteditable', value ? 'false' : 'true');
-        }
-
+    /**
+     * The model binding value of the WYSIWYG.
+     *
+     * @type {string | number | boolean | null}
+     */
+    value: {
+      type: String | Number | Boolean | null,
+      default: null
     },
-    mounted() {
-        this.editor = pell.init({
-            element: this.$refs.editor,
-            actions: this.actions,
-            defaultParagraphSeparator: this.default_paragraph_separator,
-            styleWithCSS: this.style_with_css,
-            classes: this.classes,
-            onChange: html => {
-                this.$emit('input', html);
-            }
-        });
-        this.$refs.editor
-            .querySelector('.pell-content')
-            .setAttribute('contenteditable', this.disabled ? 'false' : 'true');
-        if (this.value) {
-            this.editor.content.innerHTML = this.value;
-        }
+
+    /**
+     * WYSIWYG placeholder text.
+     *
+     * @type {string | null}
+     */
+    placeholder: {
+      type: String | null,
+      default: null
+    },
+
+    /**
+     * If the WYSIWYG is disabled and non-interactive.
+     *
+     * @type {boolean}
+     */
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+
+    /**
+     * Success subtext to display under the WYSIWYG.
+     *
+     * Subtext values override each other in the following order, only one may be present at a time:
+     *
+     * success > failure > subtext
+     *
+     * @type {string | null}
+     */
+    success: {
+      type: String | null,
+      default: null
+    },
+
+    /**
+     * Failure subtext to display under the WYSIWYG.
+     *
+     * Subtext values override each other in the following order, only one may be present at a time:
+     *
+     * success > failure > subtext
+     *
+     * @type {string | null}
+     */
+    failure: {
+      type: String | null,
+      default: null
+    },
+
+    /**
+     * Subtext to display under the WYSIWYG.
+     *
+     * Subtext values override each other in the following order, only one may be present at a time:
+     *
+     * success > failure > subtext
+     *
+     * @type {string | null}
+     */
+    subtext: {
+      type: String | null,
+      default: null
     }
+
+  },
+  data() {
+    return {
+      editor: null
+    };
+  },
+  computed: {
+
+    /**
+     * Properties to pass down to the form control component.
+     *
+     * @returns {
+     *   {parent: string},
+     *   {label: string},
+     *   {success: string},
+     *   {failure: string},
+     *   {subtext: string}
+     * }
+     */
+    properties() {
+      return {
+        parent: 'wysiwyg',
+        label: this.$slots.default ? this.$slots.default : this.label,
+        success: this.success,
+        failure: this.failure,
+        subtext: this.subtext
+      };
+    }
+
+  },
+  watch: {
+
+    /**
+     * Update the value on model changes.
+     *
+     * @param {string} value - The new value.
+     * @returns {undefined}
+     */
+    value: function (value) {
+      if (this.editor.content.innerHTML !== value) {
+        this.editor.content.innerHTML = value;
+      }
+    },
+
+    /**
+     * Adjust the disabled state.
+     *
+     * @param {string} value - The new disabled state.
+     * @returns {undefined}
+     */
+    disabled: function (value) {
+      this.$refs.editor
+        .querySelector('.pell-content')
+        .setAttribute('contenteditable', value ? 'false' : 'true');
+    }
+
+  },
+  mounted() {
+    this.editor = pell.init({
+      element: this.$refs.editor,
+      actions: this.actions,
+      defaultParagraphSeparator: this.default_paragraph_separator,
+      styleWithCSS: this.style_with_css,
+      classes: this.classes,
+      onChange: html => {
+        this.$emit('input', html);
+      }
+    });
+    this.$refs.editor
+      .querySelector('.pell-content')
+      .setAttribute('contenteditable', this.disabled ? 'false' : 'true');
+    if (this.value) {
+      this.editor.content.innerHTML = this.value;
+    }
+  }
 }
 </script>
 
@@ -217,60 +240,60 @@ export default {
 /* WYSIWYG structure */
 .aui.wysiwyg
 
-    /* Success feedback */
-    &.success .pell-actionbar, &.failure .pell-actionbar
-        border-bottom: none !important
+  /* Success feedback */
+  &.success .pell-actionbar, &.failure .pell-actionbar
+    border-bottom: none !important
 
-    /* Editor action bar area */
-    .pell-actionbar
-        display: flex
-        background: #f9f9f9
-        border-top-left-radius: 0.25rem
-        border-top-right-radius: 0.25rem
-        border: 1px solid #ced4da
+  /* Editor action bar area */
+  .pell-actionbar
+    display: flex
+    background: lighten($color-border, 15%)
+    border-top-left-radius: 0.25rem
+    border-top-right-radius: 0.25rem
+    border: 1px solid $color-border
 
-        /* Action bar buttons */
-        .pell-btn
-            display: flex
-            justify-content: center
-            align-content: center
-            font-size: 1.2rem
-            height: 2.5rem
-            line-height: 2.5rem
-            width: 2.5rem
-            background: none
-            border: none
-            outline: none
+    /* Action bar buttons */
+    .pell-btn
+      display: flex
+      justify-content: center
+      align-content: center
+      font-size: 1.2rem
+      height: 2.5rem
+      line-height: 2.5rem
+      width: 2.5rem
+      background: none
+      border: none
+      outline: none
 
-            /* Selection overrides */
-            &:hover, &.pell-button-selected
-                cursor: pointer
-                background: darken(#f9f9f9, 10%)
+      /* Selection overrides */
+      &:hover, &.pell-button-selected
+        cursor: pointer
+        background: lighten($color-border, 10%)
 
-    /* Editor content */
-    .pell-content
-        padding: 0.75rem
-        min-height: 4rem
-        border-left: 1px solid #ced4da
-        border-right: 1px solid #ced4da
-        border-bottom: 1px solid #ced4da
-        border-radius: 0 0 0.25rem 0.25rem
-        background: white
-        overflow-y: scroll
+  /* Editor content */
+  .pell-content
+    padding: 0.75rem
+    min-height: 4rem
+    border-left: 1px solid $color-border
+    border-right: 1px solid $color-border
+    border-bottom: 1px solid $color-border
+    border-radius: 0 0 0.25rem 0.25rem
+    background: white
+    overflow-y: scroll
 
-        /* Disabled editor */
-        &[contenteditable="false"]
-            cursor: not-allowed !important
-            background: darken(#e9ecef, 15%) !important
+    /* Disabled editor */
+    &[contenteditable="false"]
+      cursor: not-allowed !important
+      background: darken($color-border, 5%) !important
 
-        /* Editor focus */
-        &:focus
-            outline: none
-            box-shadow: 0 2px 5px 0 #ccc inset
+    /* Editor focus */
+    &:focus
+      outline: none
+      box-shadow: 0 2px 5px 0 #ccc inset
 
-        /* Resets */
-        p
-            margin: 0
-            padding: 0
+    /* Resets */
+    p
+      margin: 0
+      padding: 0
 
 </style>
